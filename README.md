bcbb6edf7cdb4909aebe212890728a18a581abcf
Alex Holliday <ajhollid@gmail.com>
2025-02-28 11:22:39 -0800

    add dummy data, update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React Coding Assignemnt
 
-## Instructions
+## Getting Started
 
 1. Fork the repository and clone it to your local machine.
 2. CD to `client` directory and run `npm install`
@@ -9,3 +9,15 @@
 5. Run `npm run dev` to start the server.
 6. Open the browser and navigate to `http://localhost:5173` to see the Client.
 7. Complete the assignment.
+
+## Assignment Instructions
+
+Your goal is to write a reusable, modular component that displays some data that is fetched from a server.
+
+This `DisplayComponent` should have two variants, one that displays data horizontally, one that displays data vertically.
+
+How you display the data is entirely up to you.
+
+Please follow React best practices, use hooks where it makes sense, comment liberally, etc.
+
+Commits are free so commit early and often!
--- server/index.js
+++ server/index.js
@@ -1,19 +1,39 @@
 import express from "express";
-const PORT = 59232;
+import { checksData } from "./dummyData.js";
+
+const PORT = 9800;
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+	res.send("Hello World");
 });
 
-app.get("/api/v1/whatsapp", (req, res) => {
-  res.json({
-    stauts: "ok",
-    message: "Hello World",
-  });
+app.get("/api/v1/checks", (req, res) => {
+	const random = Math.random();
+
+	if (random < 0.33) {
+		return res.status(500).json({
+			status: "error",
+			message: "Server error occurred",
+		});
+	}
+
+	if (random < 0.66) {
+		return res.json({
+			status: "ok",
+			message: "Checks fetched successfully",
+			data: [],
+		});
+	}
+
+	res.json({
+		status: "ok",
+		message: "Checks fetched successfully",
+		data: checksData,
+	});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
